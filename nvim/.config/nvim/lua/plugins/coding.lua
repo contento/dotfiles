a9b43798b4d4963424c3c2047091e69609889c09
Gonzalo Contento <3741250+contento@users.noreply.github.com>
2024-11-13 19:05:05 -0500

    add additional plugins

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -1,5 +1,14 @@
 return {
-  "smjonas/inc-rename.nvim",
-  cmd = "IncRename",
-  config = true,
+  {
+    "smjonas/inc-rename.nvim",
+    cmd = "IncRename",
+    config = true,
+  },
+
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      -- add any options here
+    },
+  },
 }
