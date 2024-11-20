44b4b5d3c1f354ed984d2baaf9111edd7bde8f4c
Gonzalo Contento <3741250+contento@users.noreply.github.com>
2024-11-20 16:17:40 -0500

    remove specific file types from render-markdown configuration

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -8,8 +8,5 @@ return {
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = {
-    file_types = { "markdown", "Avante" },
-  },
-  ft = { "markdown", "Avante" },
+  opts = {},
 }
