aa1e8613d9e838e31ab7be8b8f908a14fa0eb7dd
Gonzalo Contento <3741250+contento@users.noreply.github.com>
2024-11-20 19:46:32 -0500

    activate Enter

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -3,7 +3,6 @@ return {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       opts.completion.autocomplete = false
-      opts.mapping["<CR>"] = nil
       opts.window = {
         completion = {
           border = {
