46b14b4cdbe5737dcf538edf931e992c4d834bcc
Gonzalo Contento <3741250+contento@users.noreply.github.com>
2025-04-15 16:51:09 -0400

    refactor: update configuration files and remove unused plugins

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,19 +5,13 @@ local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 local Util = require("lazyvim.util")
 
-keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", { silent = true })
-keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", { silent = true })
-keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", { silent = true })
-keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", { silent = true })
-keymap.set("n", "<C-\\>", "<Cmd>NvimTmuxNavigateLastActive<CR>", { silent = true })
-keymap.set("n", "<C-Space>", "<Cmd>NvimTmuxNavigateNavigateNext<CR>", { silent = true })
-
 vim.keymap.set("n", "<C-/>", function()
   local is_windows = package.config:sub(1, 1) == "\\"
   local terminal_cmd = is_windows and "powershell.exe" or nil
   Util.terminal(terminal_cmd, { border = "none", opacity = 0.9 })
 end, { desc = "Toggle terminal with no border and 90% opacity" })
 
+-- Lazygit (root dir)
 vim.keymap.set("n", "<leader>gg", function()
   Util.terminal({ "lazygit" }, { cwd = Util.root(), esc_esc = false, ctrl_hjkl = false, border = "none" })
 end, { desc = "Lazygit (root dir)" })
@@ -29,10 +23,9 @@ keymap.del("n", "<C-Down>")
 keymap.del("n", "<C-Up>")
 keymap.del("n", "<C-Right>")
 
-keymap.set("n", "<M-h>", '<Cmd>lua require("tmux").resize_left()<CR>', { silent = true })
-keymap.set("n", "<M-j>", '<Cmd>lua require("tmux").resize_bottom()<CR>', { silent = true })
-keymap.set("n", "<M-k>", '<Cmd>lua require("tmux").resize_top()<CR>', { silent = true })
-keymap.set("n", "<M-l>", '<Cmd>lua require("tmux").resize_right()<CR>', { silent = true })
+-- Vim navigation
+keymap.set("n", "<C-d>", "<C-d>zz", { silent = true })
+keymap.set("n", "<C-u>", "<C-u>zz", { silent = true })
 
 -- Split windows
 keymap.set("n", "ss", ":vsplit<Return>", opts)
@@ -42,11 +35,3 @@ keymap.set("n", "sv", ":split<Return>", opts)
 keymap.set("n", "te", ":tabedit", opts)
 keymap.set("n", "<tab>", ":tabnext<Return>", opts)
 keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
-
--- telescope
-
-local function set_telescope_keymap(mode, key, action, desc)
-  vim.keymap.set(mode, key, action, { silent = true, noremap = true, desc = desc })
-end
-
-set_telescope_keymap("n", "<C-p>", "<cmd>lua require('telescope.builtin').find_files()<cr>", "Find files")
